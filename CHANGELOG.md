34bc84a3f54909e6ce25a10df29c4d9c13f266fc
Jimmy Byrd <jimmybyrd@gmail.com>
2022-03-06 00:11:18 -0500

    Bump version to 0.1.0-beta001
    ## [0.1.0-beta001] - 2022-03-06
    
    ### Added
    - Adds ColdTask and CancellableTask

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,13 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
-
-## [0.1.0] - 2017-03-17
-First release
+## [0.1.0-beta001] - 2022-03-06
 
 ### Added
-- This release already has lots of features
-
-[0.1.0]: https://github.com/user/MyCoolNewLib.git/releases/tag/v0.1.0
+- Adds ColdTask and CancellableTask
+[Unreleased]: https://github.com/TheAngryByrd/IcedTasks/compare/v0.1.0-beta001...HEAD
+[0.1.0-beta001]: https://github.com/TheAngryByrd/IcedTasks/releases/tag/v0.1.0-beta001
--- src/IcedTasks/AssemblyInfo.fs
+++ src/IcedTasks/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("IcedTasks")>]
 [<assembly: AssemblyProductAttribute("IcedTasks")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-03-06T00:00:00.0000000-05:00")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","3f298e0afc48e70a5b4097b593f7214df1186de2")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "IcedTasks"
     let [<Literal>] AssemblyProduct = "IcedTasks"
     let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-03-06T00:00:00.0000000-05:00"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
+    let [<Literal>] AssemblyMetadata_GitHash = "3f298e0afc48e70a5b4097b593f7214df1186de2"
