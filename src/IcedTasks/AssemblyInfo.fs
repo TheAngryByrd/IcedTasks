eaa4e59307a54b910d80e31855f4f50be35bdee6
Jimmy Byrd <jimmybyrd@gmail.com>
2022-11-06 23:36:40 -0500

    Bump version to 0.3.0-beta001
    ## [0.3.0-beta001] - 2022-11-06
    
    ### Fixed
    
    - [Verifies upstream F# fix for task resumption code](https://github.com/TheAngryByrd/IcedTasks/issues/8)
    
    ### Changed
    
    - [Updates to F# 7](https://github.com/TheAngryByrd/IcedTasks/issues/8)

--- src/IcedTasks/AssemblyInfo.fs
+++ src/IcedTasks/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("IcedTasks")>]
 [<assembly: AssemblyProductAttribute("IcedTasks")>]
-[<assembly: AssemblyVersionAttribute("0.2.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-03-22T00:00:00.0000000-04:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.2.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","3420057c3dedcf65db053f02aec607cf7f4eee06")>]
+[<assembly: AssemblyVersionAttribute("0.3.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-11-06T00:00:00.0000000-04:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.3.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","8c715d9290a9dcf8d840ef3eb372d9d2258bb1ce")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "IcedTasks"
     let [<Literal>] AssemblyProduct = "IcedTasks"
-    let [<Literal>] AssemblyVersion = "0.2.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-03-22T00:00:00.0000000-04:00"
-    let [<Literal>] AssemblyFileVersion = "0.2.0"
-    let [<Literal>] AssemblyInformationalVersion = "0.2.0"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "3420057c3dedcf65db053f02aec607cf7f4eee06"
+    let [<Literal>] AssemblyVersion = "0.3.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-11-06T00:00:00.0000000-04:00"
+    let [<Literal>] AssemblyFileVersion = "0.3.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.3.0"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
+    let [<Literal>] AssemblyMetadata_GitHash = "8c715d9290a9dcf8d840ef3eb372d9d2258bb1ce"
