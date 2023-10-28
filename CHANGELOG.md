ff32ef2ca7cfc0f437441a317d00c14ee85484c3
Jimmy Byrd <jimmybyrd@gmail.com>
2023-10-28 17:08:38 -0400

    Bump version to 0.8.5-beta001
    ## [0.8.5-beta001] - 2023-10-28
    
    ### Changed
    - [Reduce fsharp core version](https://github.com/TheAngryByrd/IcedTasks/pull/32)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.8.5-beta001] - 2023-10-28
+
+### Changed
+- [Reduce fsharp core version](https://github.com/TheAngryByrd/IcedTasks/pull/32)
+
 ## [0.8.4] - 2023-10-28
 
 ### Fixed
@@ -245,7 +250,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Increased soeed and lowered memory usage of ColdTask and CancellableTask
 - Build for netstandard2.0 and netstandard2.1
 
-[Unreleased]: https://github.com/TheAngryByrd/IcedTasks//compare/v0.8.4...HEAD
+[Unreleased]: https://github.com/TheAngryByrd/IcedTasks//compare/v0.8.5-beta001...HEAD
+[0.8.5-beta001]: https://github.com/TheAngryByrd/IcedTasks//compare/v0.8.4...v0.8.5-beta001
+[0.8.5-beta001]: https://github.com/TheAngryByrd/IcedTasks//compare/v0.8.4...v0.8.5-beta001
 [0.8.4]: https://github.com/TheAngryByrd/IcedTasks//compare/v0.8.3...v0.8.4
 [0.8.3]: https://github.com/TheAngryByrd/IcedTasks//compare/v0.8.2...v0.8.3
 [0.8.2]: https://github.com/TheAngryByrd/IcedTasks//compare/v0.8.0...v0.8.2
--- src/IcedTasks/AssemblyInfo.fs
+++ src/IcedTasks/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("IcedTasks")>]
 [<assembly: AssemblyProductAttribute("IcedTasks")>]
-[<assembly: AssemblyVersionAttribute("0.8.4")>]
+[<assembly: AssemblyVersionAttribute("0.8.5")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","2023-10-28T00:00:00.0000000-04:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.8.4")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.8.4")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","c72b1347ae2a926d67cc60c0d870a272c4227667")>]
+[<assembly: AssemblyFileVersionAttribute("0.8.5")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.8.5")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","90896ab06747a2574c2c96e37dbd649114c008d9")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "IcedTasks"
     let [<Literal>] AssemblyProduct = "IcedTasks"
-    let [<Literal>] AssemblyVersion = "0.8.4"
+    let [<Literal>] AssemblyVersion = "0.8.5"
     let [<Literal>] AssemblyMetadata_ReleaseDate = "2023-10-28T00:00:00.0000000-04:00"
-    let [<Literal>] AssemblyFileVersion = "0.8.4"
-    let [<Literal>] AssemblyInformationalVersion = "0.8.4"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "c72b1347ae2a926d67cc60c0d870a272c4227667"
+    let [<Literal>] AssemblyFileVersion = "0.8.5"
+    let [<Literal>] AssemblyInformationalVersion = "0.8.5"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
+    let [<Literal>] AssemblyMetadata_GitHash = "90896ab06747a2574c2c96e37dbd649114c008d9"
