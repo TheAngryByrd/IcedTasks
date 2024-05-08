9301abea2840a4e4fae2fd8785d792add5ec9b7c
Jimmy Byrd <jimmy.byrd@motivity.net>
2024-05-08 09:13:09 -0400

    Bump version to 0.11.6
    ## [0.11.6] - 2024-05-08
    
    ### Fixed
    - [Pass CancellationToken from a cancellableTask to GetAsyncEnumerator](https://github.com/TheAngryByrd/IcedTasks/pull/48) - Credits @TheAngryByrd

--- src/IcedTasks/AssemblyInfo.fs
+++ src/IcedTasks/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("IcedTasks")>]
 [<assembly: AssemblyProductAttribute("IcedTasks")>]
-[<assembly: AssemblyVersionAttribute("0.11.5")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2024-04-22T00:00:00.0000000-04:00")>]
-[<assembly: AssemblyFileVersionAttribute("0.11.5")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.11.5")>]
+[<assembly: AssemblyVersionAttribute("0.11.6")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2024-05-08T00:00:00.0000000-04:00")>]
+[<assembly: AssemblyFileVersionAttribute("0.11.6")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.11.6")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","e720a0e8984bb5808a0fdafda1c743615a119f28")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","c01c152f2dc6917194272694e9637b091e629d7b")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "IcedTasks"
     let [<Literal>] AssemblyProduct = "IcedTasks"
-    let [<Literal>] AssemblyVersion = "0.11.5"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2024-04-22T00:00:00.0000000-04:00"
-    let [<Literal>] AssemblyFileVersion = "0.11.5"
-    let [<Literal>] AssemblyInformationalVersion = "0.11.5"
+    let [<Literal>] AssemblyVersion = "0.11.6"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2024-05-08T00:00:00.0000000-04:00"
+    let [<Literal>] AssemblyFileVersion = "0.11.6"
+    let [<Literal>] AssemblyInformationalVersion = "0.11.6"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "e720a0e8984bb5808a0fdafda1c743615a119f28"
+    let [<Literal>] AssemblyMetadata_GitHash = "c01c152f2dc6917194272694e9637b091e629d7b"
