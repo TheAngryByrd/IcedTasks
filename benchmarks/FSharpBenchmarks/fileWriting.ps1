8709b9e88b9416c74fef47d66aa9574fc4f6c9cc
Jimmy Byrd <jimmy.byrd@motivity.net>
2025-03-29 12:30:44 -0400

    Convert to nuget CPM

--- benchmarks/FSharpBenchmarks/fileWriting.ps1
+++ benchmarks/FSharpBenchmarks/fileWriting.ps1
@@ -1,1 +1,1 @@
-dotnet run -c Release -f net8.0 -- --filter "*IcedTasks.Benchmarks.FileWriteBenchmarks*" --runtimes net6.0 net8.0
+dotnet run -c Release -f net8.0 -- --filter "*IcedTasks.Benchmarks.FileWriteBenchmarks*" --runtimes net9.0 net8.0
