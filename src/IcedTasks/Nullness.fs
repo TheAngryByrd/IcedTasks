5af1568780d30b26d4a8614c2b35b7a069b5daca
Jimmy Byrd <jimmy.byrd@motivity.net>
2025-06-28 14:02:01 -0400

    Refactor nullability checks in Nullness.fs to use NULLABLE directive

--- src/IcedTasks/Nullness.fs
+++ src/IcedTasks/Nullness.fs
@@ -3,21 +3,21 @@ namespace IcedTasks
 open System
 
 type ExceptionNull =
-#if NET9_0_OR_GREATER && !FABLE_COMPILER
+#if NULLABLE
     Exception | null
 #else
     Exception
 #endif
 
 type IDisposableNull =
-#if NET9_0_OR_GREATER && !FABLE_COMPILER
+#if NULLABLE
     IDisposable | null
 #else
     IDisposable
 #endif
 
 type IAsyncDisposableNull =
-#if NET9_0_OR_GREATER && !FABLE_COMPILER
+#if NULLABLE
     IAsyncDisposable | null
 #else
     IAsyncDisposable
